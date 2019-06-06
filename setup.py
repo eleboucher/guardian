febac82f986de92d8aaaa3dc4b72c7c7c0c73101
Erwan Leboucher <erwan.leboucher@gitguardian.com>
2019-06-06 16:30:15 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="Guardian",
+    version="0.0.1",
+    author_email="erwanleboucher@gmail.com",
+    description="Discord bot",
+    license="MIT",
+)
